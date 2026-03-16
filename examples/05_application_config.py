5d2cde4e7b96d1b68ccbcefae8bd8749a0854893
ChinjuPol <chinju.poil@aider.no>
2026-03-16 11:11:50 +0100

    fix :Set  Log level after timestamp

--- examples/05_application_config.py
+++ examples/05_application_config.py
@@ -20,7 +20,7 @@ from ds_common_logger_py_lib import Logger
 
 Logger.configure(
     prefix="MyApp",
-    format_string="[%(asctime)s][{prefix}][%(name)s][%(levelname)s]: %(message)s",
+    format_string="[%(levelname)s][%(asctime)s][{prefix}][%(name)s][%(filename)s:%(lineno)d]: %(message)s",
     date_format="%Y-%m-%d %H:%M:%S",
     level=logging.INFO,
     logger_levels={
--- examples/07_handler_management.py
+++ examples/07_handler_management.py
@@ -20,7 +20,7 @@ from ds_common_logger_py_lib import Logger
 
 Logger.configure(
     prefix="MyApp",
-    format_string="[%(asctime)s][{prefix}][%(name)s][%(levelname)s]: %(message)s",
+    format_string="[%(levelname)s][%(asctime)s][{prefix}][%(name)s][%(filename)s:%(lineno)d]: %(message)s",
     level=logging.INFO,
 )
 
